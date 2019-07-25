15f728074c5d38663de863e459c470cd074c1260
magechaP <magechapeter@gmail.com>
2019-07-25 10:58:44 +0300

    define test for saving one and multiple creds

--- testCredential.py
+++ testCredential.py
@@ -28,3 +28,19 @@ class TestCredential(unittest.TestCase):
         self.assertEqual(self.new_credential.username, "Sowasse")
         self.assertEqual(self.new_credential.accountname, "Pinterest")
         self.assertEqual(self.new_credential.password, "sow@yi")            
+
+    def test_save_credential(self):
+        """
+        test_save_credential test case to check whether credential is successfully saved
+        """
+        self.new_credential.save_credential()
+        self.assertEqual(len(Credential.credentials), 1)
+
+    def test_save_multiple_credentials(self):
+        """
+        test_save_multiple_credentials test case to check whether a user can save multiple credentials
+        """
+        self.new_credential.save_credential()
+        test_credential = Credential ("Lucas", "Twitter","lwairore")
+        test_credential.save_credential()
+        self.assertEqual(len(Credential.credentials), 2)        
