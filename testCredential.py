7c8433360398b88782bc5d1242373158d035c90c
magechaP <magechapeter@gmail.com>
2019-07-25 10:54:47 +0300

    import neccessary extensions and create a testcreds in test file

---
+++ testCredential.py
@@ -0,0 +1,10 @@
+import unittest     #importing unittest module
+from credential import Credential   # importing class Credential
+import pyperclip    # importing pyperclip module
+
+class TestCredential(unittest.TestCase):
+    """
+    Test class that defines the test cases for the credential class behaviours
+    Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+    """
